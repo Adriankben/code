d5d20e1c31278ebf3ff0a528eae92b8b28b56a25
Adrian Bennett <34956924+Adriankben@users.noreply.github.com>
2025-08-16 00:27:02 -0400

    Update script.js

--- javascript/countdown-timer/script.js
+++ javascript/countdown-timer/script.js
@@ -3,34 +3,35 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-// curent date
+// Current date and time
 const currentDate = new Date();
 
-//E - currentDate.getFullYear(), 9, 18
-//A - currentDate.getFullYear(), 9, 18
-// Set the date we're counting down to
-const birthdayDate = new Date(currentDate.getFullYear(), 0, 8);
+// Set the date we're counting down to (Birthday Date)
+let birthdayDate = new Date(currentDate.getFullYear(), 0, 8);
 
-// Update the count down every 1 second
+// Update the countdown every 1 second
 var x = setInterval(function() {
-
+    // Get the current time in milliseconds
     var now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    var distance = (birthdayDate - now);
-    
-    // Time calculations for days, hours, minutes and seconds
+    // Calculate the distance between now and the birthday date
+    var distance = birthdayDate - now;
+
+    // If the countdown is over, set the birthday date to the next year
+    if (distance < 0) {
+        birthdayDate.setFullYear(birthdayDate.getFullYear() + 1);
+        distance = birthdayDate - now;
+    }
+
+    // Time calculations for days, hours, minutes, and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    // Display the results in the corresponding HTML elements
     daysEl.innerHTML = days;
     hoursEl.innerHTML = hours;
     minsEl.innerHTML = mins;
     secondsEl.innerHTML = seconds;
-
-    // If the count down is over for this year, add a year and continue counting down 
-    if (distance < 0) {birthdayDate.setFullYear(birthdayDate.getFullYear() + 1)
-    }
 }, 1000);
